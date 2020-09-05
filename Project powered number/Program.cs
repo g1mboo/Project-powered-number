5430f1fbfe86e0cf8df6389d8132a3c9fbcce340
g1mboo <g1mboo@users.noreply.github.com>
2020-09-05 22:14:07 +0600

    Home work MSP - ex VCS
    Ez programm for powered numbers

---
+++ Project powered number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Project_powered_number
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Power numbers: \r\n");
+            while (true)
+            {
+                double x;
+                double y;
+
+                Console.Write("Number: ");
+                double.TryParse(Console.ReadLine(), out x);
+                Console.Write("Power: ");
+                double.TryParse(Console.ReadLine(), out y);
+
+                //Result
+                Console.WriteLine("Result: " + 
+                    await Task.Run(() => Math.Pow(x, y)));
+                Console.WriteLine("----------------");
+            }
+        }        
+    }
+}
